7a753585872f1026a675c5868b2ba0c291a3abf3
ayhanakkayagap <ayhanakkayagap20232024>
2023-11-28 16:00:45 +0300

    text widget işlendi

---
+++ lib/text_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text(
+            'Hello! How are you Muhammed?',
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 30, color: Colors.green),
+          ),
+        ));
+  }
+}
